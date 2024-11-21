beaf01d259fbf0a514acdc69874897c1d66e7ff4
Lucas Maneff <maneff@siepc-21.epfl.ch>
2024-11-21 15:25:27 +0100

    CODE LUCAS

---
+++ Market/webots/controllers/epuck_crown/build/release/epuck_crown.d
@@ -0,0 +1,12 @@
+build/release/epuck_crown.o: epuck_crown.c \
+ /usr/local/webots-2022a/include/controller/c/webots/robot.h \
+ /usr/local/webots-2022a/include/controller/c/webots/types.h \
+ /usr/local/webots-2022a/include/controller/c/webots/nodes.h \
+ /usr/local/webots-2022a/include/controller/c/webots/emitter.h \
+ /usr/local/webots-2022a/include/controller/c/webots/receiver.h \
+ /usr/local/webots-2022a/include/controller/c/webots/distance_sensor.h \
+ /usr/local/webots-2022a/include/controller/c/webots/radio.h \
+ /usr/local/webots-2022a/include/controller/c/webots/motor.h \
+ /usr/local/webots-2022a/include/controller/c/webots/supervisor.h \
+ /usr/local/webots-2022a/include/controller/c/webots/contact_point.h \
+ ../auct_super/message.h
