95a5d3bda9d75bbbcaf65ba63ee94c28f4606dca
jbenand <jacques.benand@epfl.ch>
2024-11-29 12:18:27 +0100

    nul

---
+++ Market/webots/controllers/epuck_crown/build/release/epuck_crown.d
@@ -0,0 +1,12 @@
+build/release/epuck_crown.o: epuck_crown.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/emitter.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/receiver.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/radio.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/supervisor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/contact_point.h \
+ ../auct_super/message.h
