0d7283a82bbb2e6987493112437e10bc2989ccfa
jbenand <jacques.benand@epfl.ch>
2024-12-14 12:19:30 +0100

    Three step planning functionnel

---
+++ Astolfi distributed planning/controllers/epuck_crown/build/release/epuck_crown.d
@@ -0,0 +1,13 @@
+build/release/epuck_crown.o: epuck_crown.c \
+ C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/emitter.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/receiver.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/radio.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/motor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/gps.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/supervisor.h \
+ C:\Program\ Files\Webots/include/controller/c/webots/contact_point.h \
+ ../auct_super/message.h
